78c9bba29c16fcb6b1c590ee5204c1707a5f09d0
Martin Haas <martinhaas2005@gmail.com>
2024-01-22 15:35:18 -0300

    fix: Fixed Security

---
+++ src/main/java/com/typetaskpro/config/security/SecurityFilter.java
@@ -0,0 +1,50 @@
+package com.typetaskpro.config.security;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.typetaskpro.repository.UserRepository;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+  @Autowired
+  UserRepository userRepository;
+
+  @Autowired
+  TokenService tokenService;
+
+  @Override
+  protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
+    throws IOException, ServletException{
+    
+    var token = recoverToken(request);
+    if(token != null) {
+      var login = tokenService.validateToken(token);
+      UserDetails user = userRepository.findByUsername(login);
+
+      var auth = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+      SecurityContextHolder.getContext().setAuthentication(auth);
+    }
+
+    filterChain.doFilter(request, response);
+  }
+
+
+  private String recoverToken(HttpServletRequest request) {
+    var authHeader = request.getHeader("Authorization");
+    return authHeader == null ? authHeader : authHeader.replace("Bearer ", "");
+  }
+}
